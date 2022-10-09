3a653b2f7325d061d86351ede6d9cd3be13b3ed2
kamronvaliyev <kamrovaliyev0805@gmail.com>
2022-10-09 19:33:34 +0300

    first comment

---
+++ js/app.js
@@ -0,0 +1,9 @@
+alert ("hallo my friend, welcome to my world!!!")
+var firstName = prompt("F.I.O");
+console.log(firstName);
+var secondName = prompt("yoshingiz nechida?");
+console.log(secondName);
+var thirdName = prompt("siz qayerda o`qiysiz?");
+console.log(thirdName);
+var fourthName = prompt("Sizga IT sohasi yoqadimi?")
+console.log(fourthName);
